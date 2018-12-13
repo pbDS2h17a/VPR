ab8193063246e9d6b8f92839135a3f5a38e68082
Jona Petrikowski / PBS2H17A <jona.petrikowski@edu.bib.de>
2018-12-13 15:59:26 +0100

    Player geupdatet und SqlHelper

--- src/sqlConnection/Player.java
+++ src/sqlConnection/Player.java
@@ -1,63 +1,69 @@
 //LEA-MARIE MOENIKES
+// Peroscku
 package sqlConnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.paint.Color;
 
-public class Player
-{
+public class Player {
+	
+	
 	private String name;
-	private String color;
-	private List<Country> countryList = null;
+	private Color color;
+	private List<Country> countryList;
 	private int unitsPerRound;
 	private int id;
 	
-	public Player(int id, String name, String color)
-	{
+	
+	public Player(int id, String name, String color) {
 		this.id = id;
 		this.name = name;
-		this.color = color;
 		this.countryList = new ArrayList<>();
 		this.unitsPerRound = 0;
+		
+		// extracting the three rgb-values from the string color and creating an object Color
+		String[] sa = color.split(",");
+		this.color =Color.rgb(Integer.parseInt(sa[0]),Integer.parseInt(sa[1]),Integer.parseInt(sa[2]));
 	}
 	
-	public Player(int id, String name, String color, List<Country> countryList, int units)
-	{
+	public Player(int id, String name, String color, List<Country> countryList, int units) {
 		this.id = id;
 		this.name = name;
-		this.color = color;
 		this.countryList = countryList;
 		this.unitsPerRound = units;
+		
+		// extracting the three rgb-values from the string color and creating an object Color
+		String[] sa = color.split(",");
+		this.color =Color.rgb(Integer.parseInt(sa[0]),Integer.parseInt(sa[1]),Integer.parseInt(sa[2]));
 	}
 
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getColor()
-	{
+	public Color getColor() {
 		return color;
 	}
 
-	public void setColor(String color)
-	{
+	public void setColor(Color color) {
 		this.color = color;
 	}
 	
 	
-	public int getEinheitenProRunde()
-	{
+	public int getEinheitenProRunde() {
 		return unitsPerRound;
 	}
 
-	public void setEinheitenProRunde(int einheitenProRunde)
-	{
+	public void setEinheitenProRunde (int einheitenProRunde) {
 		this.unitsPerRound = einheitenProRunde;
 	}
 
