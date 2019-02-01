5da2f00a1f7cb757927e4553d3990c8fe4b6679e
roemmich.23 <roemmich.23@gmail.com>
2019-02-01 10:38:35 +0100

    javadoc

--- src/network/ResultSetManager.java
+++ src/network/ResultSetManager.java
@@ -5,30 +5,34 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Hilfsklasse für die Rückgabewerte der Datenbank.
+ * @author Erik Schaumlöffel
+ * @author PeRoScKu
+ */
 public class ResultSetManager
 {
+	/**
+	 * Konvertiert ein ResultSet in eine List<List<String>>
+	 * @param r Auszulesendes ResultSet
+	 * @return <List<List<String>> der Abfrageergebniswerte aus dem ResultSet
+	 * @throws SQLException
+	 */
 	public static List<List<String>> toList(ResultSet r) throws SQLException
 	{
 		List<List<String>> set;
-		
-		try { if(!r.next()) throw new SQLException(); } 
+		try { r.next(); } 
 		catch (SQLException s) { return null; }
-		
 		set = new ArrayList<>();
-		
 		//Erste Zeile -> Bestimmung der Länge der Datensätze
 		List<String> zeile = new ArrayList<>();
 		int length = 1;
 		while(true) {
-			try { zeile.add(r.getString(length)); length++; }
+			try { zeile.add(r.getString(length).trim()); length++; }
 			catch (SQLException s) { break; }
-			catch (NullPointerException n) { System.out.println("???"); break;}
 		}
-		
 		if(length == 1 && zeile.size() == 0) {return null;}
-		
 		set.add(zeile);
-		
 		while(r.next())
 		{
 			zeile = new ArrayList<>();
