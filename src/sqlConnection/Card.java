4b610eb50a5a820d224f21ae8c51a3615692564a
kapacabana <>
2019-02-12 14:06:06 +0100

    Kommentierung Card, CardStack, Mission, Continent

--- src/sqlConnection/Card.java
+++ src/sqlConnection/Card.java
@@ -2,37 +2,64 @@ package sqlConnection;
 
 import java.sql.SQLException;
 
+/**
+ *	Die Klasse "Card" erstellt die Spielkarten.
+ *	@author Kai Papke
+ *	@author Jessica Young
+*/
 
 public class Card
 {
 	//Attribute
+	/**
+	 * Karten-Id als Integer
+	 * Kartenwert als Integer
+	 * Name des Landes als String
+	 */	
 	private int cardId;
 	private int value;
 	private String countryName;
 	
 	//Getters/Setters
-	public int getCardId()
-	{
+	/**
+	 * Getter Karten-Id
+	 * @return Karten-Id
+	 */
+	public int getCardId() {
 		return cardId;
 	}
-
+	
+	/**
+	 * Setter Karten-Id
+	 * @param cardId Karten-Id
+	 */
 	public void setCardID(int cardId) {
 		this.cardId = cardId;
 	}
 	
-
+	/**
+	 * Getter Name des Landes
+	 * @return Name des Landes
+	 */
 	public String getCountryName() {
 		return countryName;
 	}
 
+	/**
+	 * Setter Name des Landes
+	 * @param countryName Name des Landes
+	 */
 	public void setCountryName(String countryName) {
 		this.countryName = countryName;
 	}
 
-	
 	//Konstruktor
-	public Card(int cardId) throws SQLException 
-	{
+	/**
+	 * Holt sich von der Datenbank den Kartenwert und den Namen des Landes
+	 * @param cardId Karten-Id
+	 * @throws SQLException
+	 */
+	public Card(int cardId) throws SQLException {
 		this.cardId = cardId;
 		this.value = SqlHelper.getCardValue(cardId);		
 		this.countryName=SqlHelper.getCountryName(cardId);
@@ -56,7 +83,4 @@ public class Card
 		}
 		return "CardNo.:" + cardId + " UnitName:" + valueName+ " CountryName:" + countryName;
 	}
-
-	
-	
 }
